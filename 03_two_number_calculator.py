61af74803b8851f410e7f25d66be63b579c1f88a
Anand Sharma <anandsharma.codes@gmail.com>
2026-08-09 22:42:47 +0530

    Add Q3 Two Number Calculator

---
+++ 03_two_number_calculator.py
@@ -0,0 +1,9 @@
+# Q3 - Two Number Calculator
+
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
