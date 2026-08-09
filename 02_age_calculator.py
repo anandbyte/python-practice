867bf757fca567de2d83ffedc5034d36972826f4
Anand Sharma <anandsharma.codes@gmail.com>
2026-08-09 22:37:54 +0530

    Add Q2 Age Calculator practice

---
+++ 02_age_calculator.py
@@ -0,0 +1,8 @@
+# Q2 - Age Calculator
+
+birth_year = int(input("Enter your birth year: "))
+current_year = int(input("Enter your current year: "))
+
+age = current_year - birth_year
+
+print("Your age is:", age)
