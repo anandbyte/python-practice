fa07824759eab485745fddb16778940e00eab85b
Anand Sharma <anandsharma.codes@gmail.com>
2026-08-09 22:47:12 +0530

    Add Q5 Rectangle Area and Perimeter practice

---
+++ 05_rectangle_area_perimeter.py
@@ -0,0 +1,7 @@
+# Q5 - Rectangle Area and Perimeter
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+print("Area:", length * width)
+print("Perimeter:", 2 * (length + width))
